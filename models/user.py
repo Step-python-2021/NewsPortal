278662399a89e1f91419ac2b34bdba12f7d973de
freeker0k <freeker07dd@gmail.com>
2021-09-12 14:35:15 +0300

    Carcass added

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from  config import mysql
+
+
+class User(object):
+
+    @staticmethod
+    def register(self) -> None:
+        pass
+
+    @staticmethod
+    def login(self) -> None:
+        pass
