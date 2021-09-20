8065430ae4e7f84b85fe7679a9ee83ab2304e323
devoden <devohutsdv@gmail.com>
2021-09-20 22:09:47 +0300

    News adding tool

--- views/news_controller.py
+++ views/news_controller.py
@@ -1,14 +1,56 @@
-from flask import render_template, session
+from flask import render_template, session, request
+from werkzeug.utils import secure_filename
+from os import path
 from config import app
+from models.article import Article
+from time import strftime, localtime
 
 
 class NewsController(object):
 
     @staticmethod
-    @app.route('/news/create')
+    @app.route('/news/create', methods=['GET', 'POST'])
     def create():
         if 'user' in session and session['user'] == 'test3':
-            return render_template('news/create.html')
+            if request.method == 'GET':
+                return render_template('news/create.html')
+            elif request.method == 'POST':
+                title = request.form.get('title')
+                about = request.form.get('about')
+                content = request.form.get('content')
+                status = request.form.get('status')
+
+                file = request.files.get('image')
+                filename = secure_filename(file.filename)
+                extensions = ['png', 'jpg', 'jpeg', 'gif', 'bmp']
+                size_limit = 10 * 1024 * 1024  # 10Mb
+
+                if filename.split('.')[-1] not in extensions:
+                    message = 'Неверное расширение файла!'
+                    mess_color = 'red'
+                elif len(file.read()) > size_limit:
+                    message = f'Превышен размер файла в {size_limit} Mb'
+                    mess_color = 'red'
+                else:
+                    local_dir = path.dirname(path.abspath(__file__))
+                    root_dir = path.abspath(local_dir + '\\..\\')
+                    save_dir = root_dir + '\\media\\upload'
+                    save_path = path.join(save_dir, filename)
+                    file.stream.seek(0)
+                    file.save(save_path)
+
+                    image = f'../media/upload/{filename}'
+                    publish = strftime('%Y-%m-%d %H:%M:%S', localtime())
+                    user_id = 1  # test user id
+                    Article.add_article(title, about, content, image, publish, user_id, status)
+
+                    message = 'Новость успешно добавлена.'
+                    mess_color = 'green'
+
+                return render_template('news/create_info.html', context={
+                    'message': message,
+                    'mess_color': mess_color
+                })
         else:
             return render_template('access/page403.html')
 
